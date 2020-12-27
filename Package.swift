805d226c6adbd2f1bb5218c783fc0845cdb75e3e
kenmueller <kenmueller0@gmail.com>
2020-12-27 15:47:18 -0800

    platforms

--- Package.swift
+++ Package.swift
@@ -5,7 +5,10 @@ import PackageDescription
 let package = Package(
 	name: "socket",
 	platforms: [
-		.macOS(.v10_15)
+		.iOS(.v13),
+		.macOS(.v10_15),
+		.tvOS(.v13),
+		.watchOS(.v6)
 	],
 	products: [
 		.library(name: "Socket", targets: ["Socket"])
